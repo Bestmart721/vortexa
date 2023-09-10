fea7b4ba10390a371e14cd69500175f2e374d599
Bestmart721 <steven93721@gmail.com>
2023-09-10 19:18:09 +0530

    Removed Mock HTTPS server plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import basicSsl from "@vitejs/plugin-basic-ssl"
+// import basicSsl from "@vitejs/plugin-basic-ssl"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  vite:{
-    plugins:[basicSsl()],
-    server:{
-      https: import.meta.env.DEV,
-    }
-  }
 });
