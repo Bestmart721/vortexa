0cc887db401da76f10182f545910abdbe35a24ef
Bestmart721 <steven93721@gmail.com>
2023-09-12 01:06:07 +0530

    Added MoonPhase Icons & others misc icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,9 +3,34 @@ type WeatherImages = {
     [key: string]: ImageMetadata;
 };
 
-export { default as VortexSVG } from "./icons/vortex.svg"
-export { default as SunriseSVG } from "./icons/sunrise.svg"
-export { default as SunsetSVG } from "./icons/sunset.svg"
+// Misc Icons
+export { default as VortexSVG } from "./misc/vortex.svg"
+export { default as SunriseSVG } from "./misc/sunrise.svg"
+export { default as SunsetSVG } from "./misc/sunset.svg"
+export { default as ArrowSVG } from "./misc/arrow.svg"
+export { default as CompassSVG } from "./misc/compass.svg"
+export { default as WindSVG } from "./misc/wind.svg"
+
+// Moon Phase Icons
+import FullMoonSVG from "./misc/fullmoon.svg"
+import NewMoonSVG from "./misc/newmoon.svg"
+import FirstQuarterSVG from "./misc/first-quarter.svg"
+import LastQuarterSVG from "./misc/last-quarter.svg"
+import WanCrescentSVG from "./misc/waning-crescent.svg"
+import WanGibbousSVG from "./misc/waning-gibbous.svg"
+import WaxCrescentSVG from "./misc/waxing-crescent.svg"
+import WaxGibbousSVG from "./misc/waxing-gibbous.svg"
+
+export const MoonIcons: WeatherImages = {
+    "Full_Moon": FullMoonSVG,
+    "New_Moon": NewMoonSVG,
+    "First_Quarter": FirstQuarterSVG,
+    "Last_Quarter": LastQuarterSVG,
+    "Waning_Crescent": WanCrescentSVG,
+    "Waning_Gibbous": WanGibbousSVG,
+    "Waxing_Crescent": WaxCrescentSVG,
+    "Waxing_Gibbous": WaxGibbousSVG,
+}
 
 // Weather Logo Images
 import Day from "./icons/01d.svg"
