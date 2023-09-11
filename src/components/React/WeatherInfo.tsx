e7bc366ce95cb713029f49ac076b620dc1985431
Bestmart721 <steven93721@gmail.com>
2023-09-12 01:26:42 +0530

    Minor Changes & Moved Time info comps to new file

--- src/components/React/WeatherInfo.tsx
+++ src/components/React/WeatherInfo.tsx
@@ -1,7 +1,6 @@
 import { useStore } from '@nanostores/react'
 import { imperialUnit, weather } from '../../store/weatherStore'
-import { useEffect, useState } from 'react'
-import moment from "moment-timezone"
+import { useEffect, useState, useMemo } from 'react'
 
 // Main Temperature and location
 export const WeatherMainInfo = () => {
@@ -18,9 +17,9 @@ export const WeatherMainInfo = () => {
     if ($weather) {
         return (
             <div className="flex flex-col w-fit select-none">
-                <div className="flex_center items-start leading-none">
+                <div className="flex_center items-start leading-none min-w-[200px]">
                     <span className='text-[6em]'>
-                        {parseInt(temp?.toString()) || "undefined"}
+                        {parseInt(temp?.toString()) || "--"}
                     </span>
                     <span className='text-[4.5em]'>°{$imperialUnit ? "F" : "C"}</span>
                 </div>
@@ -38,65 +37,43 @@ export const TempDetails = () => {
     const $weather = useStore(weather)
     const $imperialUnit = useStore(imperialUnit)
 
+    const feelsLike = useMemo(() => {
+        return $imperialUnit
+            ? $weather?.current.feelslike_f || "--"
+            : $weather?.current.feelslike_c || "--";
+    }, [$imperialUnit, $weather]);
+
+    const maxTemp = useMemo(() => {
+        return $imperialUnit
+            ? $weather?.forecast.forecastday[0].day.maxtemp_f || "--"
+            : $weather?.forecast.forecastday[0].day.maxtemp_c || "--";
+    }, [$imperialUnit, $weather]);
+
+    const minTemp = useMemo(() => {
+        return $imperialUnit
+            ? $weather?.forecast.forecastday[0].day.mintemp_f || "--"
+            : $weather?.forecast.forecastday[0].day.mintemp_c || "--";
+    }, [$imperialUnit, $weather]);
+
     return (
         <div className="flex gap-8 flex-col sm:flex-row">
             <div className="flex_center flex-col p-1 rounded w-fit h-fit">
                 <span className='opacity-80'>Feels Like</span>
                 <span className='text-[1.5em]'>
-                    {$imperialUnit ?
-                        ($weather?.current.feelslike_f + "°F")
-                        : ($weather?.current.feelslike_c + "°C")
-                    }
-                </span>
-            </div>
-            <div className="flex_center flex-col p-1 rounded w-fit h-fit">
-                <span className='opacity-80'>Max Temp</span>
-                <span className='text-[1.5em]'>
-                    {$imperialUnit ?
-                        ($weather?.forecast.forecastday[0].day.maxtemp_f + "°F")
-                        : ($weather?.forecast.forecastday[0].day.maxtemp_c + "°C")
-                    }
+                    {feelsLike + ($imperialUnit ? "°F" : "°C")}
                 </span>
             </div>
             <div className="flex_center flex-col p-1 rounded w-fit h-fit">
                 <span className='opacity-80'>Min Temp</span>
                 <span className='text-[1.5em]'>
-                    {$imperialUnit ?
-                        ($weather?.forecast.forecastday[0].day.mintemp_f + "°F")
-                        : ($weather?.forecast.forecastday[0].day.mintemp_c + "°C")
-                    }
+                    {minTemp + ($imperialUnit ? "°F" : "°C")}
                 </span>
             </div>
-        </div>
-    )
-}
-
-//Formatted Time and Date
-export const TimeInfo = () => {
-    const [time, setTime] = useState<string>("")
-    const $weather = useStore(weather)
-
-    useEffect(() => {
-        if ($weather) {
-            const TimeLoop = setInterval(() => {
-                const formattedTime = moment().tz($weather?.location?.tz_id).format("LTS");
-                setTime(formattedTime);
-            }, 1000);
-
-            return () => clearInterval(TimeLoop);
-        }
-    }, [$weather]);
-
-    return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 items-center text-center">
-            <div className="flex flex-col items-start">
-                <span className='tracking-widest opacity-80 capitalize'>Today</span>
-                <span className='text-[1.8em]'>{moment().format("Do MMMM YYYY")}</span>
-            </div>
-
-            <div className="flex flex-col items-end leading-none">
-                <span className='text-[3em] tracking-wider relative'>{time}</span>
-                <span className="px-1.5 tracking-widest opacity-80 capitalize">{$weather?.location?.tz_id || "undefined"}</span>
+            <div className="flex_center flex-col p-1 rounded w-fit h-fit">
+                <span className='opacity-80'>Max Temp</span>
+                <span className='text-[1.5em]'>
+                    {maxTemp + ($imperialUnit ? "°F" : "°C")}
+                </span>
             </div>
         </div>
     )
