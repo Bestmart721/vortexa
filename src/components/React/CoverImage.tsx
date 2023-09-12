d1294765c70c5ef1770b72b483d02e27c4f2a3d3
Bestmart721 <steven93721@gmail.com>
2023-09-12 21:18:50 +0530

    Prevent Bg Img render untill imageCode is updated

--- src/components/React/CoverImage.tsx
+++ src/components/React/CoverImage.tsx
@@ -76,23 +76,24 @@ const CoverImage = ({ background }: Props) => {
         // }
     }, [$imageCode])
 
-    return (
-        <div className={background ?
-            "absolute top-0 w-screen h-screen z-[-9] overflow-hidden"
-            :
-            "absolute top-0 w-full h-full z-[-1] rounded-lg overflow-hidden"}>
-            <img
-                src={MobileImg[$imageCode]?.src}
-                alt="BG_Img"
-                draggable={false}
-                className={background ? "sm:hidden w-full h-screen fixed top-0 object-cover opacity-75" : "sm:hidden w-full h-full object-cover"} />
-            <img
-                src={DesktopImg[$imageCode]?.src}
-                alt="BG_Img"
-                draggable={false}
-                className={background ? "hidden sm:block w-full h-screen fixed top-0 object-cover opacity-75" : "hidden sm:block w-full h-full object-cover"} />
-        </div>
-    )
+    if ($imageCode !== "default")
+        return (
+            <div className={background ?
+                "absolute top-0 w-screen h-screen z-[-9] overflow-hidden"
+                :
+                "absolute top-0 w-full h-full z-[-1] rounded-lg overflow-hidden"}>
+                <img
+                    src={MobileImg[$imageCode]?.src}
+                    alt="BG_Img"
+                    draggable={false}
+                    className={background ? "sm:hidden w-full h-screen fixed top-0 object-cover opacity-75" : "sm:hidden w-full h-full object-cover"} />
+                <img
+                    src={DesktopImg[$imageCode]?.src}
+                    alt="BG_Img"
+                    draggable={false}
+                    className={background ? "hidden sm:block w-full h-screen fixed top-0 object-cover opacity-75" : "hidden sm:block w-full h-full object-cover"} />
+            </div>
+        )
 }
 
 export default CoverImage
