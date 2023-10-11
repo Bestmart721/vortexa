90566b2f2711cdfe5a33a18cfb0e3259cdd8d856
Bestmart721 <steven93721@gmail.com>
2023-10-11 18:25:37 +0530

    Added new BgImages to match IconCodes ✨🔥🎉

--- src/assets/desktop/index.ts
+++ src/assets/desktop/index.ts
@@ -6,22 +6,54 @@ type WeatherImages = {
 // Desktop Weather Images
 import D_113d from "./113d.webp"
 import D_113n from "./113n.webp"
+
 import D_116d from "./116d.webp"
 import D_116n from "./116n.webp"
+
 import D_119d from "./119d.webp"
 import D_119n from "./119n.webp"
+
 import D_122d from "./122d.webp"
 import D_122n from "./122n.webp"
-import D_176d from "./176d.webp"
-import D_176n from "./176n.webp"
-import D_296d from "./296d.webp"
-import D_296n from "./296n.webp"
-import D_200d from "./200d.webp"
-import D_200n from "./200n.webp"
-import D_179d from "./179d.webp"
-import D_179n from "./179n.webp"
+
 import D_143d from "./143d.webp"
 import D_143n from "./143n.webp"
+
+import D_176d from "./176d.webp"
+import D_176n from "./176n.webp"
+
+import D_179d from "./179d.webp"
+import D_179n from "./179n.webp"
+
+import D_182d from "./182d.webp"
+import D_182n from "./182n.webp"
+
+import D_185d from "./185d.webp"
+import D_185n from "./185n.webp"
+
+import D_200d from "./200d.webp"
+import D_200n from "./200n.webp"
+
+import D_230d from "./230d.webp"
+import D_230n from "./230n.webp"
+
+import D_277d from "./277d.webp"
+import D_277n from "./277n.webp"
+
+import D_296d from "./296d.webp"
+import D_296n from "./296n.webp"
+
+import D_305d from "./305d.webp"
+import D_305n from "./305n.webp"
+
+import D_350d from "./350d.webp"
+import D_350n from "./350n.webp"
+
+import D_389d from "./389d.webp"
+import D_389n from "./389n.webp"
+
+import D_392d from "./392d.webp"
+import D_392n from "./392n.webp"
 import D_Unknown from "./unknown.webp"
 
 export const DesktopImg: WeatherImages = {
@@ -33,15 +65,43 @@ export const DesktopImg: WeatherImages = {
     "119n": D_119n,
     "122d": D_122d,
     "122n": D_122n,
-    "176d": D_176d,
-    "176n": D_176n,
-    "296d": D_296d,
-    "296n": D_296n,
-    "200d": D_200d,
-    "200n": D_200n,
-    "179d": D_179d,
-    "179n": D_179n,
     "143d": D_143d,
     "143n": D_143n,
+    "176d": D_176d,
+    "176n": D_176n,
+    "179d": D_179d,
+    "179n": D_179n,
+    "182d": D_182d,
+    "182n": D_182n,
+    "185d": D_185d,
+    "185n": D_185n,
+    "200d": D_200d,
+    "200n": D_200n,
+    "230d": D_230d,
+    "230n": D_230n,
+    "227d": D_277d,
+    "227n": D_277n,
+    "284d": D_230d,
+    "284n": D_230n,
+    "296d": D_296d,
+    "296n": D_296n,
+    "305d": D_305d,
+    "305n": D_305n,
+    "317d": D_182d,
+    "317n": D_182n,
+    "335d": D_230d,
+    "335n": D_230n,
+    "350d": D_350d,
+    "350n": D_350n,
+    "359d": D_305d,
+    "359n": D_305n,
+    "365d": D_185d,
+    "365n": D_185n,
+    "377d": D_350d,
+    "377n": D_350n,
+    "389d": D_389d,
+    "389n": D_389n,
+    "392d": D_392d,
+    "392n": D_392n,
     "default": D_Unknown,
 }
--- src/assets/mobile/index.ts
+++ src/assets/mobile/index.ts
@@ -3,25 +3,57 @@ type WeatherImages = {
     [key: string]: ImageMetadata;
 };
 
-// Desktop Weather Images
+// Mobile Weather Images
 import M_113d from "./113d.webp"
 import M_113n from "./113n.webp"
+
 import M_116d from "./116d.webp"
 import M_116n from "./116n.webp"
+
 import M_119d from "./119d.webp"
 import M_119n from "./119n.webp"
+
 import M_122d from "./122d.webp"
 import M_122n from "./122n.webp"
-import M_176d from "./176d.webp"
-import M_176n from "./176n.webp"
-import M_296d from "./296d.webp"
-import M_296n from "./296n.webp"
-import M_200d from "./200d.webp"
-import M_200n from "./200n.webp"
-import M_179d from "./179d.webp"
-import M_179n from "./179n.webp"
+
 import M_143d from "./143d.webp"
 import M_143n from "./143n.webp"
+
+import M_176d from "./176d.webp"
+import M_176n from "./176n.webp"
+
+import M_179d from "./179d.webp"
+import M_179n from "./179n.webp"
+
+import M_182d from "./182d.webp"
+import M_182n from "./182n.webp"
+
+import M_185d from "./185d.webp"
+import M_185n from "./185n.webp"
+
+import M_200d from "./200d.webp"
+import M_200n from "./200n.webp"
+
+import M_230d from "./230d.webp"
+import M_230n from "./230n.webp"
+
+import M_277d from "./277d.webp"
+import M_277n from "./277n.webp"
+
+import M_296d from "./296d.webp"
+import M_296n from "./296n.webp"
+
+import M_305d from "./305d.webp"
+import M_305n from "./305n.webp"
+
+import M_350d from "./350d.webp"
+import M_350n from "./350n.webp"
+
+import M_389d from "./389d.webp"
+import M_389n from "./389n.webp"
+
+import M_392d from "./392d.webp"
+import M_392n from "./392n.webp"
 import M_Unknown from "./unknown.webp"
 
 export const MobileImg: WeatherImages = {
@@ -33,15 +65,43 @@ export const MobileImg: WeatherImages = {
     "119n": M_119n,
     "122d": M_122d,
     "122n": M_122n,
-    "176d": M_176d,
-    "176n": M_176n,
-    "296d": M_296d,
-    "296n": M_296n,
-    "200d": M_200d,
-    "200n": M_200n,
-    "179d": M_179d,
-    "179n": M_179n,
     "143d": M_143d,
     "143n": M_143n,
+    "176d": M_176d,
+    "176n": M_176n,
+    "179d": M_179d,
+    "179n": M_179n,
+    "182d": M_182d,
+    "182n": M_182n,
+    "185d": M_185d,
+    "185n": M_185n,
+    "200d": M_200d,
+    "200n": M_200n,
+    "230d": M_230d,
+    "230n": M_230n,
+    "227d": M_277d,
+    "227n": M_277n,
+    "284d": M_230d,
+    "284n": M_230n,
+    "296d": M_296d,
+    "296n": M_296n,
+    "305d": M_305d,
+    "305n": M_305n,
+    "317d": M_182d,
+    "317n": M_182n,
+    "335d": M_230d,
+    "335n": M_230n,
+    "350d": M_350d,
+    "350n": M_350n,
+    "359d": M_305d,
+    "359n": M_305n,
+    "365d": M_185d,
+    "365n": M_185n,
+    "377d": M_350d,
+    "377n": M_350n,
+    "389d": M_389d,
+    "389n": M_389n,
+    "392d": M_392d,
+    "392n": M_392n,
     "default": M_Unknown,
 }
